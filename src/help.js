6f530a5762a982df81a7d4e3fed306fa50eb357b
bot-rique21 <90358090+bot-rique21@users.noreply.github.com>
2021-09-09 00:11:05 -0300

    Update help.js

--- src/help.js
+++ src/help.js
@@ -1,14 +1,7 @@
 const help = (prefix, sender,time) => {
 
 	return ` 
-┏━━━━━━━━━━━━━━━━━━━━
-┃─────〘 LENDARIO 〙────
-┃━━━━━━━━━━━━━━━━━━━━
-┠⊷ numéro wa.me/${sender.split("@")[0]}   
-┠⊷ tempo ativo: ${kyun(uptime)}
-┠⊷ Velocidade: ${process.uptime()}  
-┠⊷ Data e hora: ${time}   
-┗━━━━━━━━━━━━━━━━━━━━  
+  
  ┏━━━━━━━━━━━━━━━━━━━━
  ┃─────〘 MENU 〙─────
  ┗━━━━━━━━━━━━━━━━━━━━
@@ -26,19 +19,7 @@ const help = (prefix, sender,time) => {
 ┃
 ─────〘 RIQUE O Lendario 〙─────
 ┃
-┗━━━━━━━━━━━━━━━━━━━━ 
-
-      `
-function kyun(seconds){
-  function pad(s){
-    return (s < 10 ? '0' : '') + s;
-  }
-  var Horas = Math.floor(seconds / (60*60));
-  var Minutos = Math.floor(seconds % (60*60) / 60);
-  var Segundos = Math.floor(seconds % 60);
-
-  //return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds)
-  return `${pad(Horas)} Horas ${pad(Minutos)} Minutos ${pad(Segundos)} Segundos`
+┗━━━━━━━━━━━━━━━━━━━━ ` 
 }
 }
 exports.help = help
