82b5b947e97d111aa28e4a149023b12d54107c15
bot-rique21 <90358090+bot-rique21@users.noreply.github.com>
2021-09-09 00:01:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -397,7 +397,7 @@ client.sendMessage(from, tujuh, video, {quoted: mek, mimetype: 'video/mp4', ptt:
 			 if (!isGroupAdmins) return reply(mess.only.admin)
 			if (!isGroup) return reply(`Este comando só pode ser usado em grupos`)		
 cuImg = await getBuffer (`http://2.bp.blogspot.com/-sCQOU3_sDzs/VDtCPxR-O5I/AAAAAAAAIKc/g-NV57_KTkA/s1600/Divers%C3%A3o.png`)
-client.sendMessage(from, cuImg, image, {quoted: { key: { participant: `0@s.whatsapp.net`, ...{}}, message: { "imageMessage": { "caption": "Menu ",}}}, caption: help(prefix, time, pushname, sender)})
+client.sendMessage(from, cuImg, image, {quoted: { key: { participant: `0@s.whatsapp.net`, ...{}}, message: { "imageMessage": { "caption": "Menu ",}}}, caption: help(prefix, time, sender)})
 					break
 				case '(comando)':
 	         	if (args.length < 1) return reply ('up')
