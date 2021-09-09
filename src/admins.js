77adbeb01690fd3c9695311398d2b6391bc4da67
bot-rique21 <90358090+bot-rique21@users.noreply.github.com>
2021-09-09 08:12:43 -0300

    Create admins.js

---
+++ src/admins.js
@@ -0,0 +1,37 @@
+const admins = (prefix, sender, ) => {
+
+	return`
+┏━━━━━━━━━━━━━━━━━━━━
+┃─────〘 ADMINS 〙────
+┃━━━━━━━━━━━━━━━━━━━━  
+┠⊷━━━━━━━━━━━━━━━━━━━━                              
+┠⊷ ${prefix}linkdogrupo
+┠⊷ ${prefix}remover               
+┠⊷ ${prefix}lvl   
+┠⊷ ${prefix}level               
+┠⊷ ${prefix}adiciona                 
+┠⊷ ${prefix}nivel            
+┠⊷ ${prefix}alterafoto
+┠⊷ ${prefix}alteranome     
+┠⊷ ${prefix}tiradm            
+┠⊷ ${prefix}totaladm                           
+┠⊷ ${prefix}bemvindo
+┠⊷ ${prefix}antifake
+┠⊷ ${prefix}antilink
+┠⊷ ${prefix}regras                        
+┠⊷ ${prefix}alterar  
+┠⊷ ${prefix}clonar   
+┠⊷ ${prefix}nomegp           
+┠⊷ ${prefix}fechar
+┠⊷ ${prefix}abrir
+┗━━━━━━━━━━━━━━━━━━━━
+┏━━━━━━━━━━━━━━━━━━━━
+┠⊷ Mᴇᴜ ᴄʀɪᴀᴅᴏʀ:
+┠⊷ wa.me/554891428604
+┠⊷║▌│█║▌│ █║▌│█│║▌║
+┠⊷║▌│█║▌│ █║▌│█│║▌║
+┠⊷ Copyright ® Bσт σ Lєиdαяiσ  
+┗━━━━━━━━━━━━━━━━━━━━
+`
+}
+exports.admins = admins
