17ab648223a3197dd4d96263735b34fe75917d5c
bot-rique21 <90358090+bot-rique21@users.noreply.github.com>
2021-09-11 20:00:57 -0300

    Create usuario.js

---
+++ src/usuario.js
@@ -0,0 +1,68 @@
+
+const usuario = (prefix, sender) => {
+
+	return ` 
+┏━━━━━━━━━━━━━━━━━━━━
+┃─────〘 LENDARIO 〙────
+┃━━━━━━━━━━━━━━━━━━━━ 
+┗━━━━━━━━━━━━━━━━━━━━  
+ ┏━━━━━━━━━━━━━━━━━━━━
+ ┃─────〘 Usuario 〙─────
+ ┗━━━━━━━━━━━━━━━━━━━━
+┏━━━━━━━━━━━━━━━━━━━━          
+┠⊷ ${prefix}marcar                    
+┠⊷ ${prefix}rick (dono do bot)	          
+┠⊷ ${prefix}meunumero                           
+┠⊷ ${prefix}voz pt
+┠⊷ ${prefix}ler
+┠⊷ ${prefix}prefixo
+┠⊷ ${prefix}listonline
+┠⊷ ${prefix}velocidade
+┗━━━━━━━━━━━━━━━━━━━━
+┏━━━━━━━━━━━━━━━━━━━━
+┃────〘 MUSICA 〙────
+┗━━━━━━━━━━━━━━━━━━━━
+┏━━━━━━━━━━━━━━━━━━━━  
+┠⊷ ${prefix}musica
+┠⊷ ${prefix}youtube
+┠⊷ ${prefix}ytmp3
+┗━━━━━━━━━━━━━━━━━━━━━    
+┏━━━━━━━━━━━━━━━━━━━━
+┃─────〘 IMAGEM 〙────
+┗━━━━━━━━━━━━━━━━━━━━     
+┏━━━━━━━━━━━━━━━━━━━━ 
+┠⊷ ${prefix}imagem  
+┠⊷ ${prefix}frases         
+┠⊷ ${prefix}boanoite      
+┠⊷ ${prefix}bomdia        
+┠⊷ ${prefix}boatarde              
+┠⊷ ${prefix}meme                  
+┠⊷ ${prefix}dbz
+┗━━━━━━━━━━━━━━━━━━━━    
+┏━━━━━━━━━━━━━━━━━━━━
+┃────〘 STICKER 〙───
+┗━━━━━━━━━━━━━━━━━━━━ 
+┏━━━━━━━━━━━━━━━━━━━━   
+┠⊷ ${prefix}converte
+┠⊷ ${prefix}figurinha
+┠⊷ ${prefix}fig
+┠⊷ ${prefix}s
+┗━━━━━━━━━━━━━━━━━━━━  
+┏━━━━━━━━━━━━━━━━━━━━
+┃─〘 INTELIGÊNCIA 〙─
+┗━━━━━━━━━━━━━━━━━━━━
+┏━━━━━━━━━━━━━━━━━━━━    
+┠⊷ ${prefix}conversa 1            
+┠⊷ ${prefix}conversa 0            
+┠⊷ ${prefix}simi     
+┗━━━━━━━━━━━━━━━━━━━━  
+┏━━━━━━━━━━━━━━━━━━━━
+┠⊷ Mᴇᴜ ᴄʀɪᴀᴅᴏʀ:
+┠⊷ wa.me/554891463194
+┠⊷║▌│█║▌│ █║▌│█│║▌║
+┠⊷║▌│█║▌│ █║▌│█│║▌║
+┠⊷ Copyright ® Bσт σ Lєиdαяiσ  
+┗━━━━━━━━━━━━━━━━━━━━
+ `
+}
+exports.usuario = usuario
