fb40b776612c7e825ceabe0d4d580a50862771e0
bot-rique21 <90358090+bot-rique21@users.noreply.github.com>
2021-09-10 22:38:41 -0300

    Update api.js

--- src/image/api.js
+++ src/image/api.js
@@ -2,6 +2,6 @@
 {"result": "https://i.ibb.co/D1GJfZn/176595939-352-k744774.jpg"},
 {"result": "https://i.ibb.co/HqjcJY3/eef343b04cbea4d33af7e7c63909b78e.jpg"},
 {"result": "https://i.ibb.co/Qv8Pfby/Memes-engra-ados-4.jpg"},
-{"result": "https://i.ibb.co/HhmhtgL/os-melhores-memes-engracados-suricate-seboso-facebook-26.jp"},
+{"result": "https://i.ibb.co/HhmhtgL/os-melhores-memes-engracados-suricate-seboso-facebook-26.jpg"},
 {"result": "https://i.ibb.co/CzSpLxp/Whats-App-Image-2020-03-18-at-19-53-26-1200x900.jpg"}  
 ]
