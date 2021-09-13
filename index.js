488d879efdcbc228e20b33a22ab26900ac41483d
bot-rique21 <90358090+bot-rique21@users.noreply.github.com>
2021-09-12 23:13:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -594,64 +594,7 @@ async function starts() {
 
 		//FUNÇÃO PRO BOT FALAR ACABOU
 		
-			if (budy.includes("https://m.kwai.me/")){
-		if (!isGroup) return
-		if (!isAntiBucin) return
-		if (isGroupAdmins) return reply('porque você é um administrador do grupo, e o bot  não vai te remover')
-		client.updatePresence(from, Presence.composing)
-		if (messagesC.includes("#harusizintod")) return reply("izin diterima")
-		var kic = `${sender.split("@")[0]}@s.whatsapp.net`
-		reply(`「 ANTI-KWAIII DETECTADO 」\n${sender.split("@")[0]} voce sera expulso*`)
-		setTimeout( () => {
-			client.groupRemove(from, [kic]).catch((e)=>{reply(`ERR: ${e}`)})
-		}, 1000)
-		setTimeout( () => {
-			client.updatePresence(from, Presence.composing)
-		}, 0)
-	}
-        if (budy.includes("https://s.kwai.app/s/")){
-		if (!isGroup) return
-		if (!isAntiBucin) return
-		if (isGroupAdmins) return reply('porque você é um administrador do grupo, os bot não grupo ')
-		client.updatePresence(from, Presence.composing)
-		if (messagesC.includes("#harusizintod")) return reply("izin diterima")
-		var kic = `${sender.split("@")[0]}@s.whatsapp.net`
-		reply(`「 SPAM DETECTADO 」\n${sender.split("@")[0]} vou remover por postar links kwai *`)
-		setTimeout( () => {
-			client.groupRemove(from, [kic]).catch((e)=>{reply(`ERR: ${e}`)})
-		}, 1000)
-		setTimeout( () => {
-			client.updatePresence(from, Presence.composing)
-		
-		}, 0)
-	}
-			if (budy.includes("https://chat.whatsapp.com/")){
-		if (!isGroup) return
-		if (!isAntiBucin) return
-		if (isGroupAdmins) return reply('porque você é um administrador do grupo, e o bot  não vai te remover')
-		client.updatePresence(from, Presence.composing)
-		if (messagesC.includes("#harusizintod")) return reply("izin diterima")
-		var kic = `${sender.split("@")[0]}@s.whatsapp.net`
-		setTimeout( () => {
-			client.groupRemove(from, [kic]).catch((e)=>{reply(`ERR: ${e}`)})
-		}, 1000)
-		setTimeout( () => {
-			client.updatePresence(from, Presence.composing)
-		
-		}, 0)
-			}
-			const reply = (teks) => {
-				client.sendMessage(from, teks, text, {quoted:mek})
-			}
-			const sendImage = (teks) => {
-		    client.sendMessage(from, teks, image, {quoted:mek})
-		    }
-			const sendMess = (hehe, teks) => {
-				client.sendMessage(hehe, teks, text)
-			}
-			const mentions = (teks, memberr, id) => {
-				(id == null || id == undefined || id == false) ? client.sendMessage(from, teks.trim(), extendedText, {contextInfo: {"mentionedJid": memberr}}) : client.sendMessage(from, teks.trim(), extendedText, {quoted: mek, contextInfo: {"mentionedJid": memberr}})
-			}
+			
               //function leveling
             if (isGroup && isLevelingOn) {
             const currentLevel = getLevelingLevel(sender)
