9eb5f495be2e530afa8bb4079f00de69b20ce64e
bot-rique21 <90358090+bot-rique21@users.noreply.github.com>
2021-09-13 18:49:07 -0300

    Update help.js

--- src/help.js
+++ src/help.js
@@ -1,6 +1,16 @@
-const help = (prefix, sender) => {
+const help = (prefix, sender, pushname, time, frasealeatoria) => {
 
 	return ` 
+┏━━━━━━━━━━━━━━━━━━━━
+┃─────〘 LENDARIO 〙────
+┃━━━━━━━━━━━━━━━━━━━━
+┠⊷ nome: ${pushname}
+┠⊷ numéro wa.me/${sender.split("@")[0]}   
+┠⊷ tempo ativo: ${kyun(uptime)}
+┠⊷ Velocidade: ${process.uptime()}  
+┠⊷ Data e hora: ${time}   
+┠⊷ frase: ${frasealeatoria}
+┗━━━━━━━━━━━━━━━━━━━━  
  ┏━━━━━━━━━━━━━━━━━━━━
  ┃─────〘 MENU 〙─────
  ┗━━━━━━━━━━━━━━━━━━━━
@@ -12,7 +22,20 @@ const help = (prefix, sender) => {
 ┠⊷ ${prefix}diversao
 ┗━━━━━━━━━━━━━━━━━━━━
  ┏━━━━━━━━━━━━━━━━━━━━
- ┃─────〘 BOT O LENDARIO 〙─────
- ┗━━━━━━━━━━━━━━━━━━━━ ` 
+ ┃─────〘 LєиdαяiσBσт 〙─────
+ ┗━━━━━━━━━━━━━━━━━━━━ 
+
+      `
+function kyun(seconds){
+  function pad(s){
+    return (s < 10 ? '0' : '') + s;
+  }
+  var Horas = Math.floor(seconds / (60*60));
+  var Minutos = Math.floor(seconds % (60*60) / 60);
+  var Segundos = Math.floor(seconds % 60);
+
+  //return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds)
+  return `${pad(Horas)} Horas ${pad(Minutos)} Minutos ${pad(Segundos)} Segundos`
+}
 }
 exports.help = help
