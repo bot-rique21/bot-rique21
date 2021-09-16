fa949fa585905cd9d21ff17fd43978892a4a0a9b
bot-rique21 <90358090+bot-rique21@users.noreply.github.com>
2021-09-16 20:04:17 -0300

    Update frases.js

--- src/image/frases.js
+++ src/image/frases.js
@@ -1,9 +1,9 @@
 [
-{"result": "link"},
-{"result": "link"},
-{"result": "link"},
-{"result": "link"},
-{"result": "link"},
-{"result": "link"},
-{"result": "link"}  
+{"result": "https://ereasson.sirv.com/frases.api/1.jpg"},
+{"result": "https://ereasson.sirv.com/frases.api/2.jpg"},
+{"result": "https://ereasson.sirv.com/frases.api/3.jpg"},
+{"result": "https://ereasson.sirv.com/frases.api/4.jpg"},
+{"result": "https://ereasson.sirv.com/frases.api/5.jpg"},
+{"result": "https://ereasson.sirv.com/frases.api/6.jpg"},
+{"result": "https://ereasson.sirv.com/frases.api/7.jpg"}  
 ]
