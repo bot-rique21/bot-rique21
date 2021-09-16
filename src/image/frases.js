9b44cd99d78ca9709172ac60ce712f906813cbc2
bot-rique21 <90358090+bot-rique21@users.noreply.github.com>
2021-09-16 20:20:52 -0300

    Update frases.js

--- src/image/frases.js
+++ src/image/frases.js
@@ -1,4 +1,4 @@
 [
 {"result": "Um coração feliz é o resultado inevitável de um coração ardente de amor."},
- {"result": "https://ereasson.sirv.com/frases.api/1.jpg"}
+ {"result": "O amor não se vê com os olhos mas com o coração."}
 ]
