41218889999d829de7984a4369d5952aaa6b8200
bot-rique21 <90358090+bot-rique21@users.noreply.github.com>
2021-09-16 20:15:30 -0300

    Update frases.js

--- src/image/frases.js
+++ src/image/frases.js
@@ -1,9 +1,4 @@
 [
-{"result": "https://ereasson.sirv.com/frases.api/1.jpg"},
-{"result": "https://ereasson.sirv.com/frases.api/2.jpg"},
-{"result": "https://ereasson.sirv.com/frases.api/3.jpg"},
-{"result": "https://ereasson.sirv.com/frases.api/4.jpg"},
-{"result": "https://ereasson.sirv.com/frases.api/5.jpg"},
-{"result": "https://ereasson.sirv.com/frases.api/6.jpg"},
-{"result": "https://ereasson.sirv.com/frases.api/7.jpg"}  
+{"result": "Um coração feliz é o resultado inevitável de um coração ardente de amor."},
+ {"result": "https://ereasson.sirv.com/frases.api/1.jpg"}
 ]
